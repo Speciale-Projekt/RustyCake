beed535fc099801c15dc5b3177e9292451fd7d20
Lars Bo Frydenskov <lb@innobeat.dk>
2022-06-21 10:51:32 +0200

    File read on notify

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate notify;
 extern crate libafl;
 
+use std::borrow::Borrow;
+use std::fs;
 use libafl::{
     bolts::AsSlice,
     inputs::{BytesInput, HasTargetBytes},
 };
-use notify::{Watcher, RecursiveMode, watcher};
+use notify::{Watcher, RecursiveMode, watcher, raw_watcher, RawEvent, op};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use std::fs::File;
@@ -14,43 +16,37 @@ use std::io::prelude::*;
 use crate::libafl::inputs::Input;
 
 fn main() {
-
-    let mut harness = |input: &BytesInput| {
-        // Create a channel to receive the events.
-        let (tx, rx) = channel();
-
-        // Create a watcher object, delivering debounced events.
-        // The notification back-end is selected based on the platform.
-        let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
-
-        // Add a path to be watched. All files and directories at that path and
-        // below will be monitored for changes.
-        watcher.watch("oof.tx", RecursiveMode::Recursive).unwrap();
-
-        input.to_file("foo.txt");
-
-        loop {
-            match rx.recv() {
-                Ok(event) => match event {
-                    CLOSE_WRITE => {
-                        let mut inputFile = File::open("oof.txt").unwrap();
-                        let mut reader = BufReader::new(inputFile);
-                        let mut buffer = Vec::new();
-
-                        // Read file into vector.
-                        reader.read_to_end(&mut buffer);
-                        if buffer[0..1] == [0x00;0x11] {
-                            panic!("123");
-                        }
-                    }
-                    _ => continue
-                },
-                Err(e) => println!("watch error: {:?}", e),
-            }
-        }
-    };
-    let mut discovery = b"\xff\x10\x1a\x04\x80\x02\x18\x00";
-    let input = BytesInput::new(discovery.to_vec());
-    harness(&input);
-
+    monitor_file("test");
 }
+
+fn monitor_file(dir_path: &str){
+    let mut filename: String = dir_path.to_owned();
+    filename.push_str("/child.bin");
+
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Create a watcher object, delivering debounced events.
+    // The notification back-end is selected based on the platform.
+    let mut watcher = raw_watcher(tx).unwrap();
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(dir_path, RecursiveMode::Recursive).unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(RawEvent{path: Some(path), op: Ok(op),cookie}) =>{
+                match op {
+                     op::WRITE=> {
+                         let contents = fs::read(filename.borrow()).expect("Something went wrong reading the file");
+                    },
+                    _ => {}
+                }
+            },
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+}
+
